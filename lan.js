14804791458afdff338192e70f03eba3449cec6f
daniloHung <danilo.hung@cienet.com>
2024-01-07 16:17:37 +0800

    [Updated] 0107

---
+++ lan.js
@@ -0,0 +1,9 @@
+const mainContent = [
+  `於2023年起在Cienet擔任前端工程師，參與的工作內容包含既有網頁系統的維護以及網頁功能的開發與測試。熟悉的基礎技術包含HTML, CSS, JS, TS，其中JS Library與Frame work主要使用Angular以及React，CSS框架則以BootStrap為主。<br>
+    此外，因應嵌入式AI的發展與趨勢，近期亦研究VSCode Extension的開發，並且實際應用至工作內容中協助團隊操作自動化流程之業務，因此亦熟悉VSCode API, VSCode Extension Development之技術，並足以依相關需求獨立開發VSCode擴展應用程式。<br><br>
+    在擔任軟體工程師以前，曾任職於AECOM都市設計師，並於第二年起擔任Key Designer。<br>
+    工作的內容主要帶領團隊成員共同執行專案，管理專案進度執行以及業主(/客戶)的溝通協調，確保項目進度穩定執行。這樣的工作經歷下，使我熟悉與他人共同工作，與團隊共同為目標一起精進技術與前進。`,
+
+  `I am a Front-End Engineer at Cienet since 2023, skilled in HTML, CSS, JavaScript, and TypeScript, with a focus on Angular and React. My work involves enhancing web systems and developing new functionalities. Recently, I've expanded my expertise to include VSCode Extension development, aligning with emerging AI trends. This has enabled me to contribute to automating and optimizing team workflows.
+    Before software engineering, I was an Urban Designer at AECOM, evolving into a Key Designer. This experience honed my skills in project management and team leadership, valuable in my current tech role. My career combines technical prowess with a flair for innovative solutions, aiming to continually advance the field of web development.`,
+];
